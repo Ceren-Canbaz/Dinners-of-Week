77236c3bdcd33cd0ed2dc410aa88cc8ed9d4141d
Ceren Canbaz <ceren_canbaz@hotmail.com>
2023-11-08 00:17:21 +0300

    save

--- lib/presentation/team_home_page.dart
+++ lib/presentation/team_home_page.dart
@@ -1,18 +1,38 @@
 import 'package:dinners_of_week/presentation/teams_page.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class TeamHomePage extends StatelessWidget {
+class TeamHomePage extends StatefulWidget {
   const TeamHomePage({super.key});
 
+  @override
+  State<TeamHomePage> createState() => _TeamHomePageState();
+}
+
+class _TeamHomePageState extends State<TeamHomePage> {
   @override
   Widget build(BuildContext context) {
     final params =
         ModalRoute.of(context)!.settings.arguments as TeamHomePageParameters;
     return Scaffold(
       appBar: AppBar(
-        actions: [TextButton(onPressed: () {}, child: Text("aaa"))],
+        title: Text("Teams"),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                final prefs = await SharedPreferences.getInstance();
+
+                await prefs.remove('dowUsername');
+                setState(() {});
+              },
+              icon: Icon(Icons.exit_to_app))
+        ],
       ),
-      body: ListView(children: [Text("test ${params.user.isAdmin}")]),
+      body: ListView(children: [
+        Text(
+          "test ${params.user.isAdmin}",
+        )
+      ]),
     );
   }
 }
