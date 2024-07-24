d705e2045628e2322c977c063e96fd223b927be7
Ceren Canbaz <ceren_canbaz@hotmail.com>
2024-07-24 11:48:18 +0300

    Custom exceptions and failures added

---
+++ lib/utils/exceptions/exceptions.dart
@@ -0,0 +1,5 @@
+class ServerException implements Exception {}
+
+class AuthException implements Exception {}
+
+class ParsingException implements Exception {}
---
+++ lib/utils/failures/failure.dart
@@ -0,0 +1,9 @@
+abstract class Failure {}
+
+class ServerFailure extends Failure {}
+
+class AuthFailure extends Failure {}
+
+class ParsingFailure extends Failure {}
+
+class UnknownFailure extends Failure {}
